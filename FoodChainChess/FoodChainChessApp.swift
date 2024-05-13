d2d492918fa7cae337d36919811836da707b2646
Nico <nicolas.franco@etu.uca.fr>
2024-05-13 11:32:52 +0200

    :tada: created project + colorsets

---
+++ FoodChainChess/FoodChainChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodChainChessApp.swift
+//  FoodChainChess
+//
+//  Created by étudiant on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodChainChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
