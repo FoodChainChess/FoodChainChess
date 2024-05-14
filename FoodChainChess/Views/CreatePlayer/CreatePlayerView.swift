738e4c81d5714c7fac12d384edebf69ded57b3cd
Bruno_MAC <brunocunha1506@gmail.com>
2024-05-14 11:49:10 +0200

    Feat: new files added to CreatePlayer

---
+++ FoodChainChess/Views/CreatePlayer/CreatePlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  CreatePlayerView.swift
+//  FoodChainChess
+//
+//  Created by étudiant on 14/05/2024.
+//
+
+import SwiftUI
+
+struct CreatePlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreatePlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreatePlayerView()
+    }
+}
---
+++ FoodChainChess/Views/CreatePlayer/ProfilePictureView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfilePictureView.swift
+//  FoodChainChess
+//
+//  Created by étudiant on 14/05/2024.
+//
+
+import SwiftUI
+
+struct ProfilePictureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfilePictureView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilePictureView()
+    }
+}
