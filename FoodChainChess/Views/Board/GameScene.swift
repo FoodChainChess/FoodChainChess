27d6b2fe8bc6ed5076c33ca1f9b55759762d0c79
Lou BRODA <Lou.BRODA@etu.uca.fr>
2024-05-31 09:21:36 +0200

    ADD : disposition de tous les jetons sur le plateau

--- FoodChainChess/Views/Board/GameScene.swift
+++ FoodChainChess/Views/Board/GameScene.swift
@@ -5,23 +5,43 @@ import DouShouQiModel
 class GameScene : SKScene{
     
     let imageBoard : SKSpriteNode =
-        SKSpriteNode(imageNamed: "Board")
+    SKSpriteNode(imageNamed: "Board")
     let meepleSize = CGSize(width: 80, height: 80)
     
-    var game: Game?
+    var game: Game
+    let player1: Player = Player(withName: "Lou", andId: .player1)!
+    let player2: Player = Player(withName: "LouBis", andId: .player2)!
+    
     var pieces: [Owner: [Animal: SpriteMeeple]] = [:]
     
     override init(size: CGSize) {
+        let players = [player1, player2]
+        let animals: [Animal] = [.rat, .cat, .dog, .wolf, .leopard, .tiger, .lion, .elephant]
+        self.game = try! Game(withRules: ClassicRules(), andPlayer1: self.player1, andPlayer2: self.player2)
         super.init(size: size)
+        
         self.scaleMode = .aspectFit
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
         self.addChild(imageBoard)
-                
-        self.pieces = [
-            .player1: [.rat: SpriteMeeple(imageNamed: "rat", size: meepleSize, backgroundColor: .yellow)],
-            .player2: [.rat: SpriteMeeple(imageNamed: "rat", size: meepleSize, backgroundColor: .red)]
-        ]
+        
+        for player in players {
+            self.pieces[player.id] = [:]
+            for animal in animals {
+                let color: UIColor
+                switch player.id {
+                case .player1:
+                    color = .red
+                case .player2:
+                    color = .yellow
+                default:
+                    color = .gray
+                }
+                self.pieces[player.id]?[animal] = SpriteMeeple(imageNamed: "\(animal)", size: meepleSize, backgroundColor: color)
+            }
+        }
+            
+        print(pieces)
         
         for piece in pieces.flatMap( {owner, pieces in
             pieces.values
@@ -31,12 +51,12 @@ class GameScene : SKScene{
         
         do {
             self.game = try Game(withRules: ClassicRules(), andPlayer1: HumanPlayer(withName: "Human", andId: .player1)!, andPlayer2: RandomPlayer(withName: "Random", andId: .player2)!)
-            displayBoard(game!.board)
+            displayBoard(game.board)
         } catch {
             print("Failed to initialize game: \(error)")
         }
         
-        displayBoard(game!.board)
+        displayBoard(game.board)
     }
     
     func displayBoard(_ board: Board ) {
@@ -50,6 +70,7 @@ class GameScene : SKScene{
     }
     
     required init?(coder aDecoder: NSCoder) {
+        self.game = try! Game(withRules: ClassicRules(), andPlayer1: self.player1, andPlayer2: self.player2)
         super.init(coder: aDecoder)
     }
 }
