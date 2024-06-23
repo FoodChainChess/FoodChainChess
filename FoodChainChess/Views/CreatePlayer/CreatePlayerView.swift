a571a660b343165f78f6a4c38eb3f01da471798a
Nico <nicolas.franco@etu.uca.fr>
2024-06-23 21:37:16 +0200

    Image picker working

--- FoodChainChess/Views/CreatePlayer/CreatePlayerView.swift
+++ FoodChainChess/Views/CreatePlayer/CreatePlayerView.swift
@@ -3,9 +3,7 @@ import SwiftUI
 struct CreatePlayerView: View {
     @State private var name = ""
     @State private var errorMessage = ""
-    
     @State private var isShowingConfirmation = false
-    
     var playerManager = PlayerManager.shared
     
     var body: some View {
@@ -19,7 +17,7 @@ struct CreatePlayerView: View {
                 .background(Colors.backgroundbutton)
                 .cornerRadius(10)
                 .shadow(radius: 2)
-                .padding(.horizontal, 60) // Changer la taille lateral
+                .padding(.horizontal, 60)
                 .padding(.top, 50)
             
             if !errorMessage.isEmpty {
@@ -41,23 +39,24 @@ struct CreatePlayerView: View {
                     Alert(
                         title: Text("New player created.", tableName: "Localization"),
                         message: Text("Player \(name) was created.", tableName: "Localization"),
-
                         dismissButton: .default(
                             Text("Ok", tableName: "Localization"),
                             action: {
                                 self.name = ""
                             }
-                        ))
+                        )
+                    )
                 }
-            }.padding(60)
+            }
+            .padding(60)
             Spacer()
-        }.background(Colors.background)
+        }
+        .background(Colors.background)
     }
 }
 
 struct CreatePlayerView_Previews: PreviewProvider {
     static var previews: some View {
-    
         CreatePlayerView()
     }
 }
--- FoodChainChess/Views/CreatePlayer/ProfilePictureView.swift
+++ FoodChainChess/Views/CreatePlayer/ProfilePictureView.swift
@@ -1,18 +1,23 @@
 import SwiftUI
 
 struct ProfilePictureView: View {
-    @State private var image = UIImage()
+    @State private var image = UIImage(named: "defaultAvatarPicture")!
     @State private var showSheet = false
     
     var body: some View {
-        Image("defaultAvatarPicture")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 200, height: 200)
-            .cornerRadius(100)
-            .sheet(isPresented: $showSheet) {
-                ImagePicker(sourceType: .camera, selectedImage: self.$image)
-            }
+        VStack {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .cornerRadius(100)
+                .onTapGesture {
+                    self.showSheet = true
+                }
+                .sheet(isPresented: $showSheet) {
+                    ImagePicker(selectedImage: self.$image, sourceType: .camera)
+                }
+        }
     }
 }
 
